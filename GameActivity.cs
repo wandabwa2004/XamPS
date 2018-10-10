f640abcba66413fd3bc14b796e62cba52ec0e965
Herman Masindano Wandabwa <hwandabw@aut.ac.nz>
2018-10-10 18:20:01 +1300

    Add project files.

---
+++ GameActivity.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace XamPaperScissorsRock
+{
+    [Activity(Label = "Paper Scissors Rock")]
+
+    //https://developer.xamarin.com/guides/android/user_interface/form_elements/radiobutton/ Radiobutton code
+
+    public class GameActivity : Activity
+    {
+        private ImageView GamePic;
+        private string Name;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            //load the game screen
+            SetContentView(Resource.Layout.Game);
+
+            Name = Intent.GetStringExtra("Name");
+
+
+            TextView txtMessage = FindViewById<TextView>(Resource.Id.tvName);
+            txtMessage.Text = "Choose an option " + Name;
+            GamePic = FindViewById<ImageView>(Resource.Id.imageAnswer);
+
+            //Radiobutton binding
+            RadioButton RPaper = FindViewById<RadioButton>(Resource.Id.radio_paper);
+            RadioButton RScissors = FindViewById<RadioButton>(Resource.Id.radio_scissors);
+            RadioButton RRock = FindViewById<RadioButton>(Resource.Id.radio_rock);
+
+            //radiobuttons going to same click event
+            RPaper.Click += RadioButtonClick;
+            RScissors.Click += RadioButtonClick;
+            RRock.Click += RadioButtonClick;
+
+        }
+
+        //this method runs the entire game
+        private void RadioButtonClick(object sender, EventArgs e)
+        {
+            RadioButton rb = (RadioButton)sender;
+
+            string comp = ComputerChoice();
+            string Hum = rb.Text;
+            //   Toast.MakeText(this, Name + " " + Hum + " vrs Comp = " + comp, ToastLength.Long).Show();
+
+            if (Hum == "Paper" && comp == "Rock"
+                || Hum == "Scissors" && comp == "Paper"
+                || Hum == "Rock" && comp == "Scissors")
+
+            {
+                GamePic.SetImageResource(Resource.Drawable.win);
+                Toast.MakeText(this, "Won!", ToastLength.Long).Show();
+            }
+            else if (Hum == comp)
+
+            {
+
+                //  GamePic.SetImageResource(Resource.Drawable.ww2);
+                Toast.MakeText(this, "Draw", ToastLength.Long).Show();
+            }
+
+            else
+            {
+                //  GamePic.SetImageResource(Resource.Drawable.lose);
+                Toast.MakeText(this, "Suck on that " + Name, ToastLength.Long).Show();
+
+            }
+        }
+
+        public string ComputerChoice()
+        {
+            //create a new instance of the Random Class
+            var CompGuess = new Random();
+            //This code generates a random integer between 1 and 4, but 4 is not inclusive, meaning the only possibilities are 1, 2 and 3
+            //1 represents paper, 2 represents scissors, 3 represents rock 
+            string[] Guess = { "", "Paper", "Scissors", "Rock" };
+            return Guess[CompGuess.Next(1, 4)];
+        }
+
+    }
+}
---
+++ MainActivity.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Widget;
+using Android.OS;
+
+namespace XamPaperScissorsRock
+{
+    [Activity(Label = "Paper Scissors Rock", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        //create your local variables
+        private Button btnNext;
+        private TextView txtName;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+            Init();
+        }
+
+        private void Init()
+        {
+            //bind the local variable to the layout controls
+            txtName = FindViewById<TextView>(Resource.Id.etEnterName);
+            btnNext = FindViewById<Button>(Resource.Id.btnNext);
+            //create a button click
+            btnNext.Click += onNext_Click;
+        }
+
+        private void onNext_Click(object sender, EventArgs e)
+        {
+            //Someone forgot to add a name so you have to catch it
+            if (txtName.Text == string.Empty)
+            {
+                Toast.MakeText(this, "Add a name first", ToastLength.Long).Show();
+                return;
+            }
+
+            //toast check to see its working
+            Toast.MakeText(this, "Your name is " + txtName.Text, ToastLength.Long).Show();
+            //create an intent - which is what you intend to do
+            var gameActivity = new Intent(this, typeof(GameActivity));
+            //when the intent run pass across some extra data
+            gameActivity.PutExtra("Name", txtName.Text);
+            //run the intent and start the other screen passing over the data
+            StartActivity(gameActivity);
+        }
+    }
+}
